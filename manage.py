5eabeae7135c8a15603e96f67f3600e4a015dffc
aleanse dos santos lima rego <aleanse_54@hotmail.com>
2024-02-14 00:55:02 -0300

    R

--- manage.py
+++ manage.py
@@ -4,13 +4,13 @@ import os
 import sys
 
 
-from dotenv import load_dotenv
+#from dotenv import load_dotenv
 
 
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
-    load_dotenv()
+    #load_dotenv()
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
