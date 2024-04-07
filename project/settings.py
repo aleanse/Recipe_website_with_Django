9869a9a20c65575ed32a5c23a6ab1e823741d2fb
aleanse dos santos lima rego <aleanse_54@hotmail.com>
2024-04-07 20:24:55 -0300

    tirando debug_to_bar

--- project/settings.py
+++ project/settings.py
@@ -40,13 +40,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'recipes',
     'authors',
-    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
 
     'django.middleware.security.SecurityMiddleware',
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -150,8 +148,3 @@ MESSAGE_TAGS = {
     constants.SUCCESS: 'message-success',
     constants.WARNING: 'message-warning',
 }
-
-# django debug_tool_bar
-INTERNAL_IPS = [
-    "127.0.0.1",
-]
