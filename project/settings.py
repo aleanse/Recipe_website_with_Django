dc3caf1ad3f42a5fd81d1cccd023e8e9fd00035e
aleanse dos santos lima rego <aleanse_54@hotmail.com>
2024-07-29 20:58:52 -0300

    preparando-deploy-vercel

--- project/settings.py
+++ project/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
         'USER': os.getenv('DATABASE_USER'),
         'PASSWORD': os.getenv('DATABASE_PASSWORD'),
         'HOST': os.getenv('DATABASE_HOST'),
-        'PORT': os.getenv('DATABASE_PORT'),
+        'PORT': '5432',
         'OPTIONS': {
             'sslmode': 'require',
         },
