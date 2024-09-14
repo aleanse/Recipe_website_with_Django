803740ca2674117d864517cdb49d1d030b19820e
aleanse dos santos lima rego <aleanse_54@hotmail.com>
2024-09-13 23:32:49 -0300

    update .gitignore

--- project/settings.py
+++ project/settings.py
@@ -27,9 +27,9 @@ load_dotenv()
 SECRET_KEY = os.getenv('SECRET_KEY', 'INSECURE')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = [".vercel.app"]
 
 
 # Application definition
